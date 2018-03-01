3ba8018b1142673294b2878b96878bd950d16b45
Jpulver94 <jpulver94@gmail.com>
2018-03-01 13:05:47 -0800

    added postmates

--- js/main.js
+++ js/main.js
@@ -1,28 +1,4 @@
-// GONNA DO A CLOCK HERE //
-//days 0-6 0 is sunday
-//hours 0-23
-//minutes 0 - 59
-//seconds 0 - 59
-
-/* MUSASHI HOURS
-Mon 11:30 am - 2:30 pm
-5:00 pm - 9:00 pm 
-Tue 11:30 am - 2:30 pm
-5:00 pm - 9:00 pm 
-Wed 11:30 am - 2:30 pm
-5:00 pm - 9:00 pm
-Thu 11:30 am - 2:30 pm
-5:00 pm - 9:00 pm 
-Fri 11:30 am - 2:30 pm
-5:00 pm - 9:30 pm 
-Sat 5:00 pm - 9:30 pm 
-Sun Closed*/
-// if (getday == 0) closed 
-// else{
-// if (hour >= 11 && minutes >= 30) && (hour <= 14 && minutes <=30) then is open
-// if (hour >= 17) && (hour <= 21) then is open
-// }
-// Clock feature here
+// Header tells Open or Closed
  function isOpen() {
   var date = new Date();
   var openClose = document.getElementById("openOrNot");
@@ -46,7 +22,7 @@ Sun Closed*/
 
   } else if( 14 <= hour < 17 ){ // if hours are between 2 and 5 
     if ( hour == 14 ) { // if hour is 2 
-      if ( min <= 30 ){
+      if ( min <= 30 ){ // if its before 2:30
         openClose.innerHTML = "Open Now!";
       } else openClose.innerHTML = "Closed until 5:00pm";
     } else openClose.innerHTML = "Closed until 5:00pm";
