b5fadecab0a2d2cce0348c310fee4c6ae3aff5d5
Jpulver94 <jpulver94@gmail.com>
2018-03-01 16:53:19 -0800

    working on context hours

--- js/main.js
+++ js/main.js
@@ -1,44 +1,22 @@
 // Header tells Open or Closed
  function isOpen() {
+
   var date = new Date();
   var openClose = document.getElementById("openOrNot");
   var day = date.getDay();
   var hour = date.getHours();
   var min = date.getMinutes();
   var sec = date.getSeconds();
+  openClose.innerHTML = "hello";
   if ( day == 0 ) {
     openClose.innerHTML = "Closed until Monday";
-  } else if (day == 5){ //Friday
-      if ( 11 <= hour <= 14 ) ){ // if hours are between 11 and 2 inclusive
-        openClose.innerHTML = "Open Now!"; // set first to open
-        if ( hour == 11 ){
-          if ( min >= 30 ){
-            openClose.innerHTML = "Open Now!"; //Open after 11:30
-          } else openClose.innerHTML = "Closed until 11:30am"; // Closed before 11:30
-        } 
-        else if ( hour == 14 ){
-          if ( min < 30 ) {
-            openClose.innerHTML = "Open Now!"; //open before 2:30
-          } else openClose.innerHTML = "Closed until 5:00pm"; // closed after 2:30 til 5p
-        }
-      }
-      else if( 14 <= hour < 17 ){ // if hours are between 2 and 5 
-        openClose.innerHTML = "Closed until 11:00am"; // set first to closed
-        if ( hour == 14 ) { // if hour is 2 
-          if ( min <= 30 ){ // if its before 2:30
-            openClose.innerHTML = "Open Now!";
-          } else openClose.innerHTML = "Closed until 5:00pm";
-        }
-      } else if ( (hour >= 17) && (hour <= 21) ){ // open between 5 and 9:30
-          if ( hour != 21 ) openClose.innerHTML = "Open Now!";
-          else if ( min < 30 ) openClose.innerHTML = "Open Now!";
-          else openClose.innerHTML = "Closed until 11:00am";
-        } 
-   }// end friday 
+  }
 
-   // DO SATURDAY!!!
-  //MON thru THURS
-  else if ( 11 <= hour <= 14 ) ){ // if hours are between 11 and 2 inclusive
+  // Friday
+  else if (day == 5){
+     // if hours are between 11 and 2 inclusive
+    if ( 11 <= hour <= 14 ){
+      openClose.innerHTML = "Open Now!"; // set first to open
       if ( hour == 11 ){
         if ( min >= 30 ){
           openClose.innerHTML = "Open Now!"; //Open after 11:30
@@ -48,17 +26,60 @@
         if ( min < 30 ) {
           openClose.innerHTML = "Open Now!"; //open before 2:30
         } else openClose.innerHTML = "Closed until 5:00pm"; // closed after 2:30 til 5p
-      } else openClose.innerHTML = "Open Now!"; // Must be between 11:30 and 2:30
+      }
+    }
+    // if hours are between 2 and 5 
+    else if( 14 <= hour < 17 ){ 
+      openClose.innerHTML = "Closed until 11:00am"; // set first to closed
+      if ( hour == 14 ) { // if hour is 2 
+        if ( min <= 30 ){ // if its before 2:30
+          openClose.innerHTML = "Open Now!";
+        } else openClose.innerHTML = "Closed until 5:00pm";
+      }
+    } 
+    // if hours are between 5 and 9 
+    else if ( (hour >= 17) && (hour <= 21) ){ 
+        if ( hour != 21 ) openClose.innerHTML = "Open Now!";
+        else if ( min < 30 ) openClose.innerHTML = "Open Now!";
+        else openClose.innerHTML = "Closed until 11:00am";
+      } 
+   } // end friday 
 
-  } else if( 14 <= hour < 17 ){ // if hours are between 2 and 5 
+   // DO SATURDAY!!!
+
+  //MON thru THURS
+  // if hours are between 11 and 2 
+  else if ( 11 <= hour <= 14 ){
+    openClose.innerHTML = "Open Now!"; // set first to open
+    if ( hour == 11 ){
+      if ( min >= 30 ){
+        openClose.innerHTML = "Open Now!"; //Open after 11:30
+      } else openClose.innerHTML = "Closed until 11:30am"; // Closed before 11:30
+    } 
+    else if ( hour == 14 ){
+      if ( min < 30 ) {
+        openClose.innerHTML = "Open Now!"; //open before 2:30
+      } else openClose.innerHTML = "Closed until 5:00pm"; // closed after 2:30 til 5p
+    }
+  } 
+  // if hours are between 2 and 5 
+  else if( 14 <= hour < 17 ){
+    openClose.innerHTML = "Closed until 5:00pm"; // set first to close
     if ( hour == 14 ) { // if hour is 2 
       if ( min <= 30 ){ // if its before 2:30
         openClose.innerHTML = "Open Now!";
       } else openClose.innerHTML = "Closed until 5:00pm";
-    } else openClose.innerHTML = "Closed until 5:00pm";
-  } else if ( (hour >= 17) && (hour <= 21) ){
+    }
+  }
+  // if hours are between 5 and 9 
+  else if ( 17 <= hour < 21  ){
       openClose.innerHTML = "Open Now!";
-  } else { 
+  } 
+  // it's closed
+  else { 
       openClose.innerHTML = "Closed until 11:00am"; }
+  //set color
+  openClose.style.color = "#a30b0b";
 }
+
 isOpen();
