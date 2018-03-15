3fa838008b304e123f4c108316759733668b16a1
Maggie <mchan8248@gmail.com>
2018-03-15 12:04:01 -0700

    hide menu

--- js/main.js
+++ js/main.js
@@ -17,44 +17,44 @@
 
   // Friday
   if (day == 5){
-    // if hours are between 11 and 2 
+    // if hours are between 11 and 2
     if ( hour >= 11 && hour <= 14 ){
       openClose.innerHTML = "Open until 2:30pm!"; // set first to open
       if ( hour == 11 ){
         if ( min >= 30 ){
           openClose.innerHTML = "Open until 2:30pm!"; //Open after 11:30
         } else openClose.innerHTML = "Closed until 11:30am"; // Closed before 11:30
-      } 
+      }
       else if ( hour == 14 ){
         if ( min < 30 ) {
           openClose.innerHTML = "Open until 2:30pm!"; //open before 2:30
         } else openClose.innerHTML = "Closed until 5:00pm"; // closed after 2:30 til 5p
       }
     }
-    // if hours are between 3 and 5 (caught case of 2:30 in last if) 
-    else if ( hour > 14 && hour < 17 ){ 
+    // if hours are between 3 and 5 (caught case of 2:30 in last if)
+    else if ( hour > 14 && hour < 17 ){
       openClose.innerHTML = "Closed until 5:00pm"; // set first to closed
-    } 
-    // if hours are between 5 and 9 
-    else if ( (hour >= 17) && (hour <= 21) ){ 
+    }
+    // if hours are between 5 and 9
+    else if ( (hour >= 17) && (hour <= 21) ){
         if ( hour != 21 ) openClose.innerHTML = "Open until 9:30pm!";
         else if ( min < 30 ) openClose.innerHTML = "Open until 9:30pm!";
         else openClose.innerHTML = "Closed until 11:00am";
-      } 
-   } // end friday 
+      }
+   } // end friday
 
   // Saturday
   if ( day == 6 ){
     // set first to close
     openClose.innerHTML = "Closed unitl Monday";
-    // if hours are between 5 and 9 
+    // if hours are between 5 and 9
     if ( hour >= 17 && hour < 21  ){
         openClose.innerHTML = "Open until 9:30pm!";
         if ( hour == 21 ) {
           if ( min < 30 ) openClose.innerHTML = "Open  until 9:30pm!";
           else openClose.innerHTML = "Closed until Monday";
         }
-    } 
+    }
   }
 
   //MON thru THURS
@@ -66,24 +66,34 @@
         if ( min >= 30 ){
           openClose.innerHTML = "Open until 2:30pm!"; //Open after 11:30
         } else openClose.innerHTML = "Closed until 11:30am"; // Closed before 11:30
-      } 
+      }
       else if ( hour == 14 ){
         if ( min < 30 ) {
           openClose.innerHTML = "Open until 2:30pm!"; //open before 2:30
         } else openClose.innerHTML = "Closed until 5:00pm"; // closed after 2:30 til 5p
       }
-    } 
-    // if hours are between 3 and 5 
+    }
+    // if hours are between 3 and 5
     else if( hour > 14 && hour < 17 ){
       openClose.innerHTML = "Closed until 5:00pm";
     }
-    // if hours are between 5 and 9 
+    // if hours are between 5 and 9
     else if ( hour >= 17 && hour < 21  ){
         openClose.innerHTML = "Open until 9:30pm!";
-    } 
+    }
   }
   //set color
   openClose.style.color = "FireBrick";
 }
 
 isOpen();
+
+//menu
+function changeMenu() {
+  var x = document.getElementById("menu");
+  if (x.style.display === "none") {
+      x.style.display = "block";
+  } else {
+      x.style.display = "none";
+  }
+}
